5bded42e5189dd230a5db67e87479f308d95fe18
KamalDGRT <kamaldgrt@gmail.com>
2022-06-28 17:24:25 +0530

    initial setup

---
+++ GreenGrocery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GreenGrocery
+//
+//  Created by Kamal Sharma R on 28/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
